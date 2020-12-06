fe0c97baa2e513ba861951ac36ec111e988a27a7
Bruce <graham.clodge@gmail.com>
2020-12-06 08:52:34 -0500

    Finished day six

---
+++ Advent.2020.Tests/DailyTests/Day.06.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using Advent._2020.Classes;
+
+namespace Advent._2020.Tests.DailyTests
+{
+    [TestClass]
+    public class Day06 : IDailyTests
+    {
+        public int Number => 6;
+
+        public string InputFile => TestHelper.GetInputFile(this);
+        public string TestFile => TestHelper.GetTestFile(this);
+
+        [TestMethod]
+        public void PartOne()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile);
+            var groups = Utility.Functions.SplitByByElement(input, "")
+                                          .Select(grp => new CustomsGroup(grp))
+                                          .ToList();
+
+            int num = groups.Sum(x => x.CountYes);
+
+            Assert.IsTrue(num == 7283);
+        }
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile);
+            var groups = Utility.Functions.SplitByByElement(input, "")
+                                          .Select(grp => new CustomsGroup(grp))
+                                          .ToList();
+
+            int num = groups.Sum(x => x.CountAllYes);
+
+            Assert.IsTrue(num == 3520);
+        }
+    }
+}
---
+++ Advent.2020/Classes/CustomsForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent._2020.Classes
+{
+    public class CustomsAnswer
+    {
+        public Dictionary<char, int> Results { get; } = null;
+
+        public CustomsAnswer(string input)
+        {
+            Results = new Dictionary<char, int>();
+            foreach (var c in input)
+            {
+                if (!Results.ContainsKey(c))
+                {
+                    Results.Add(c, 0);
+                }
+                Results[c]++;
+            }
+        }
+    }
+
+    public class CustomsGroup
+    {
+        public List<CustomsAnswer> Answers { get; } = null;
+
+        public int CountYes => CountDistinctYes();
+        public int CountAllYes => CountIfAllYes();
+
+        public int GroupSize => Answers.Count;
+
+        public CustomsGroup(IEnumerable<string> answers)
+        {
+            this.Answers = answers.Select(x => new CustomsAnswer(x)).ToList();
+        }
+
+        private int CountDistinctYes()
+        {
+            var yes = new HashSet<char>();
+            foreach (var answer in Answers)
+            {
+                foreach (var c in answer.Results.Keys)
+                {
+                    yes.Add(c);
+                }
+            }
+            return yes.Count;
+        }
+
+        private int CountIfAllYes()
+        {
+            var yesDict = new Dictionary<char, int>();
+            foreach (var answer in Answers)
+            {
+                foreach (var c in answer.Results.Keys)
+                {
+                    if (!yesDict.ContainsKey(c))
+                    {
+                        yesDict.Add(c, 0);
+                    }
+                    yesDict[c]++;
+                }
+            }
+            return yesDict.Keys.Where(c => yesDict[c] == GroupSize).Count();
+        }
+    }
+}
