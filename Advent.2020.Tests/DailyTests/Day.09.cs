b4c734d4353b0618503e04ae069697fc8c09e09b
Bruce <graham.clodge@gmail.com>
2020-12-09 10:58:21 -0500

    Finished day nine

---
+++ Advent.2020.Tests/DailyTests/Day.09.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using Advent._2020.Classes;
+
+namespace Advent._2020.Tests.DailyTests
+{
+    [TestClass]
+    public class Day09 : IDailyTests
+    {
+        public int Number => 9;
+
+        public string InputFile => TestHelper.GetInputFile(this);
+        public string TestFile => TestHelper.GetTestFile(this);
+
+        [TestMethod]
+        public void Test_KnownPreamble()
+        {
+            var input = Helpers.FileHelper.ParseFile(TestFile, long.Parse);
+            var xmas = new XmasEncoder(input, 5);
+
+            var badVal = xmas.CheckFirstInvalidValue();
+            Assert.IsTrue(badVal.HasValue && badVal.Value == 127);
+        }
+
+        private const long _PartOneResult = 15353384;
+        [TestMethod]
+        public void PartOne()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile, long.Parse);
+            var xmas = new XmasEncoder(input, 25);
+
+            var badVal = xmas.CheckFirstInvalidValue();
+            Assert.IsTrue(badVal.HasValue && badVal == _PartOneResult);
+        }
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile, long.Parse);
+            var xmas = new XmasEncoder(input, 25);
+
+            var weakness = xmas.FindEncryptionWeakness(_PartOneResult);
+            Assert.IsTrue(weakness.HasValue && weakness == 2466556);
+        }
+    }
+}
---
+++ Advent.2020/Classes/Encoding.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Advent._2020.Classes
+{
+    using PreambleMap = Dictionary<long, List<Tuple<long, long>>>;
+
+    public class XmasEncoder
+    {
+        public List<long> Inputs { get; } = null;
+
+        public int Index { get; private set; } = 0;
+        public int PreambleSize { get; private set; } = 25;
+
+        public long CurrentValue => Inputs[Index];
+
+        public List<long> Preamble => Inputs.GetRange(Index - PreambleSize, PreambleSize);
+        public PreambleMap PreambleMap => GetPreambleMap(Preamble);
+
+        public XmasEncoder(IEnumerable<long> inputs, int preambleSize)
+        {
+            this.Inputs = inputs.ToList();
+            this.PreambleSize = preambleSize;
+        }
+
+        public long? CheckFirstInvalidValue()
+        {
+            //< Reset the starting index to right after the preamble
+            Index = PreambleSize;
+            //< Loop over inputs and see which doesn't have a match in the pre-amble map
+            while (Index < Inputs.Count)
+            {
+                if (!PreambleMap.ContainsKey(CurrentValue))
+                {
+                    return CurrentValue;
+                }
+                Index++;
+            }
+            //< Didn't encounter any without matching sums in preamble -> return null
+            return null;
+        }
+
+        public long? FindEncryptionWeakness(long sumTarget)
+        {
+            Index = 0;
+
+            List<long> vals = null;
+            bool bFoundMatch = false;
+            //< Want to loop over indices looking for contiguous sets that sum to target
+            while (Index < Inputs.Count && !bFoundMatch)
+            {
+                vals = new List<long>();
+                for (int i = Index; i < Inputs.Count; i++)
+                {
+                    //< Current sum less than target -> add new value and progress
+                    if (vals.Sum() < sumTarget)
+                    {
+                        vals.Add(Inputs[i]);
+                    }
+                    //< Match found -> break
+                    else if (vals.Sum() == sumTarget)
+                    {
+                        bFoundMatch = true;
+                        break;
+                    }
+                    //< Current sum is greater than target -> move on
+                    else
+                    {
+                        break;
+                    }
+                }
+                Index++;
+            }
+
+            if (bFoundMatch)
+            {
+                long weakness = vals.Min() + vals.Max();
+                return weakness;
+            }
+            else
+                return null;
+        }
+
+        public static PreambleMap GetPreambleMap(IEnumerable<long> preamble)
+        {
+            var map = new PreambleMap();
+            for (int i = 0; i < preamble.Count(); i++)
+            {
+                long a = preamble.ElementAt(i);
+                for (int j = 0; j < preamble.Count(); j++)
+                {
+                    if (i != j)
+                    {
+                        long b = preamble.ElementAt(j);
+                        long sum = a + b;
+                        if (!map.ContainsKey(sum))
+                        {
+                            map.Add(sum, new List<Tuple<long, long>>());
+                        }
+                        map[sum].Add(Tuple.Create(a, b));
+                    }
+                }
+            }
+            return map;
+        }
+    }
+}
