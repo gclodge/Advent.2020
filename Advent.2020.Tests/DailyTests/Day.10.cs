7dcb016b7bcf19ebff9f9d25ec4922d1136cf6eb
Bruce <graham.clodge@gmail.com>
2020-12-10 06:27:45 -0500

    Finished day ten

---
+++ Advent.2020.Tests/DailyTests/Day.10.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using Advent._2020.Classes;
+
+namespace Advent._2020.Tests.DailyTests
+{
+    [TestClass]
+    public class Day10 : IDailyTests
+    {
+        public int Number => 10;
+
+        public string InputFile => TestHelper.GetInputFile(this);
+        public string TestFile => TestHelper.GetTestFile(this);
+
+        public static List<int> KnownAdapters = new List<int>()
+        {
+            16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4
+        };
+
+        [TestMethod]
+        public void Test_KnownAdapters()
+        {
+            var arr = new JoltageArray(KnownAdapters);
+            var diffs = arr.GetAdapterArrayDifferences();
+
+            Assert.IsTrue(diffs.Count == 2);
+            Assert.IsTrue(diffs[1] == 7);
+            Assert.IsTrue(diffs[3] == 5);
+        }
+
+        [TestMethod]
+        public void PartOne()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile, int.Parse).ToList();
+            var arr = new JoltageArray(input);
+
+            var diffs = arr.GetAdapterArrayDifferences();
+            int res = diffs[1] * diffs[3];
+
+            Assert.IsTrue(res == 2170);
+        }
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile, int.Parse).ToList();
+            var arr = new JoltageArray(input);
+
+            long combos = arr.CountPossibleCombinations();
+            Assert.IsTrue(combos == 24803586664192);
+        }
+    }
+}
---
+++ Advent.2020/Classes/Joltage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Advent._2020.Classes
+{
+    public class JoltageAdapter
+    {
+        public int Rating { get; } = 0;
+        public int MinRating => Rating - 3;
+
+        public JoltageAdapter(int rating)
+        {
+            Rating = rating;
+        }
+
+        public bool ConnectsTo(JoltageAdapter other)
+        {
+            int diff = (this.Rating - other.Rating);
+            return (diff > 0 && diff <= 3);
+        }
+    }
+
+    public class JoltageArray
+    {
+        public List<JoltageAdapter> Adapters { get; } = null;
+
+        public JoltageAdapter ChargingOutlet { get; } = new JoltageAdapter(0);
+        public JoltageAdapter BuiltIn => new JoltageAdapter(Adapters.Max(x => x.Rating));
+
+        public JoltageArray(List<int> input)
+        {
+            //< Get the (sorted) array of JoltageAdapters
+            Adapters = input.Select(x => new JoltageAdapter(x)).OrderBy(x => x.Rating).ToList();
+        }
+
+        public Dictionary<int, int> GetAdapterArrayDifferences()
+        {
+            //< Instantiate the result map with the 'built-in' difference of +3 pre-populated
+            var resMap = new Dictionary<int, int>()
+            {
+                { 3, 1 },
+            };
+
+            JoltageAdapter curr = ChargingOutlet;
+            for (int i = 0; i < Adapters.Count; i++)
+            {
+                if (Adapters[i].ConnectsTo(curr))
+                {
+                    int dJ = Adapters[i].Rating - curr.Rating;
+                    if (!resMap.ContainsKey(dJ))
+                    {
+                        resMap.Add(dJ, 0);
+                    }
+                    resMap[dJ]++;
+                    curr = Adapters[i];
+                }
+            }
+
+            return resMap;
+        }
+
+        public long CountPossibleCombinations()
+        {
+            var adaptsWithCharger = Adapters;
+            adaptsWithCharger.Insert(0, new JoltageAdapter(0));
+
+            var steps = new long[adaptsWithCharger.Count];
+            steps[0] = 1;
+
+            //< Skipping the first (already set to 1) and last (zero connections)
+            foreach (var i in Enumerable.Range(1, Adapters.Count - 1))
+            {
+                foreach (var j in Enumerable.Range(0, i))
+                {
+                    if (adaptsWithCharger[i].Rating - adaptsWithCharger[j].Rating <= 3)
+                    {
+                        steps[i] += steps[j];
+                    }
+                }
+            }
+
+            return steps.Last();
+        }
+    }
+}
