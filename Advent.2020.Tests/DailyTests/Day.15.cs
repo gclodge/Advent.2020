018a99cd39c4caf50b4913ef4e3c84b2c411b2bb
Bruce <graham.clodge@gmail.com>
2020-12-15 11:53:41 -0500

    Finished day fifteen

---
+++ Advent.2020.Tests/DailyTests/Day.15.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Advent._2020.Classes;
+
+namespace Advent._2020.Tests.DailyTests
+{
+    [TestClass]
+    public class Day15 : IDailyTests
+    {
+        public int Number => 15;
+
+        public static readonly int[] Input = new int[] { 1, 20, 11, 6, 12, 0 };
+
+        #region Test Cases
+        public static readonly List<Tuple<int[], int>> KnownResults_2020 = new List<Tuple<int[], int>>()
+        {
+            Tuple.Create(new int[] { 1, 3, 2 }, 1),
+            Tuple.Create(new int[] { 2, 1, 3 }, 10),
+            Tuple.Create(new int[] { 1, 2, 3 }, 27),
+            Tuple.Create(new int[] { 2, 3, 1 }, 78),
+            Tuple.Create(new int[] { 3, 2, 1 }, 438),
+            Tuple.Create(new int[] { 3, 1, 2 }, 1836),
+        };
+
+        public static readonly List<Tuple<int[], int>> KnownResults_30mil = new List<Tuple<int[], int>>()
+        {
+            Tuple.Create(new int[] { 0, 3, 6 }, 175594),
+            Tuple.Create(new int[] { 1, 3, 2 }, 2578),
+            Tuple.Create(new int[] { 2, 1, 3 }, 3544142),
+            Tuple.Create(new int[] { 1, 2, 3 }, 261214),
+            Tuple.Create(new int[] { 2, 3, 1 }, 6895259),
+            Tuple.Create(new int[] { 3, 2, 1 }, 18),
+            Tuple.Create(new int[] { 3, 1, 2 }, 362),
+        };
+        #endregion
+
+        [TestMethod]
+        public void Test_KnownResults_2020()
+        {
+            int target = 2020;
+            foreach (var tup in KnownResults_2020)
+            {
+                var word = MemoryGame.FindWordAtTurn(tup.Item1, target);
+                Assert.IsTrue(word == tup.Item2);
+            }
+        }
+
+        [TestMethod]
+        [Ignore] //< Ignored for now as it takes ~18s for all cases to run but gets the correct result
+        public void Test_KnownResults_30mil()
+        {
+            int target = 30000000;
+            foreach (var tup in KnownResults_30mil)
+            {
+                var word = MemoryGame.FindWordAtTurn(tup.Item1, target);
+                Assert.IsTrue(word == tup.Item2);
+            }
+        }
+
+        [TestMethod]
+        public void PartOne()
+        {
+            int target = 2020;
+            int word = MemoryGame.FindWordAtTurn(Input, target);
+
+            Assert.IsTrue(word == 1085);
+        }
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            int target = 30000000;
+            int word = MemoryGame.FindWordAtTurn(Input, target);
+            //< ~2s run time, not ideal but functional for the day
+            Assert.IsTrue(word == 10652);
+        }
+    }
+}
---
+++ Advent.2020/Classes/MemoryGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Advent._2020.Classes
+{
+    public class MemoryGame
+    {
+        public static int FindWordAtTurn(IEnumerable<int> input, int target)
+        {
+            var wordMap = new Dictionary<int, int>();
+            int len = input.Count();
+            //< Pre-populate the word map with the first occurence of the input (save the last number)
+            for(int i = 0; i < len - 1; i++)
+            {
+                //< Map has the word as the key with the value being the 'turn' it was last spoken (here, i+1)
+                wordMap.Add(input.ElementAt(i), i + 1);
+            }
+            //< Grab the 'last' word in the input sequence as the starting 'last' word spoken
+            int lastWord = input.Last();
+            for (int turn = len + 1; turn <= target; turn++) //< Loop until we hit our turn target (lawd, save my efficiency)
+            {
+                //< Check if the word's been spoken before
+                if (!wordMap.ContainsKey(lastWord))
+                {
+                    wordMap[lastWord] = turn - 1; //< Was spoken on the last turn (turn - 1)
+                    lastWord = 0;                 //< First time this word was spoken, thus next value is zero
+                }
+                else
+                {
+                    //< Get the difference between when it was last spoken
+                    int newWord = (turn - 1) - wordMap[lastWord];
+                    //< Update the map to last time spoken
+                    wordMap[lastWord] = (turn - 1);
+                    //< Set the 'last' word spoken to the newly calculated difference
+                    lastWord = newWord;
+                }
+            }
+            //< Return the last spoken word, assuming this actually terminates
+            return lastWord;
+        }
+    }
+}
