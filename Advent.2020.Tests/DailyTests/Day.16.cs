0037e4388151eac5c28ca088a0c5bb8d835e6546
Bruce <graham.clodge@gmail.com>
2020-12-16 11:48:31 -0500

    Finished day sixteen

---
+++ Advent.2020.Tests/DailyTests/Day.16.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Advent._2020.Classes;
+
+namespace Advent._2020.Tests.DailyTests
+{
+    [TestClass]
+    public class Day16 : IDailyTests
+    {
+        public int Number => 16;
+
+        public string InputFile => TestHelper.GetInputFile(this);
+        public string TestFile => TestHelper.GetTestFile(this);
+
+        [TestMethod]
+        public void Test_KnownTickets()
+        {
+            var input = Helpers.FileHelper.ParseFile(TestFile);
+
+            var manager = new TicketManager(input);
+            int error = manager.GetScanningErrorRate();
+
+            Assert.IsTrue(error == 71);
+        }
+
+        [TestMethod]
+        public void Test_KnownFields()
+        {
+            var input = Helpers.FileHelper.ParseFile(TestHelper.GetTestFile(this, "Test2"));
+
+            var manager = new TicketManager(input);
+            var positions = manager.GetValidFieldPositions();
+            var values = manager.GetMyTicketValues(positions);
+
+            Assert.IsTrue(values["row"] == 11);
+            Assert.IsTrue(values["class"] == 12);
+            Assert.IsTrue(values["seat"] == 13);
+        }
+
+        [TestMethod]
+        public void PartOne()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile);
+
+            var manager = new TicketManager(input);
+            int error = manager.GetScanningErrorRate();
+
+            Assert.IsTrue(error == 29759);
+        }
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            var input = Helpers.FileHelper.ParseFile(InputFile);
+
+            var manager = new TicketManager(input);
+            var positions = manager.GetValidFieldPositions();
+            var values = manager.GetMyTicketValues(positions);
+
+            string target = "departure";
+            //< Want the values that start with 'departure'
+            var targetValues = values.Where(kvp => kvp.Key.StartsWith(target)).ToList();
+            //< Ensure that it's only six values
+            Assert.IsTrue(targetValues.Count == 6);
+
+            long finalVal = 1;
+            foreach (var kvp in targetValues)
+            {
+                finalVal *= kvp.Value;
+            }
+
+            Assert.IsTrue(finalVal == 1307550234719);
+        }
+    }
+}
---
+++ Advent.2020/Classes/TrainTicket.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Advent._2020.Classes
+{
+    public class TicketNote
+    {
+        public string Name { get; } = null;
+        public List<int[]> Ranges { get; } = null;
+
+        public TicketNote(string line)
+        {
+            var arr = line.Split(new string[] { ": " }, StringSplitOptions.None);
+            this.Name = arr[0];
+
+            var rangeData = arr[1].Split(new string[] { " or " }, StringSplitOptions.None);
+            this.Ranges = rangeData.Select(x => ParseRange(x)).ToList();
+        }
+
+        private static int[] ParseRange(string data)
+        {
+            return data.Split('-').Select(x => int.Parse(x)).ToArray();
+        }
+    }
+
+    public class Ticket
+    {
+        public List<int> Values { get; } = null;
+        public int Length => Values.Count;
+
+        public Ticket(string line)
+        {
+            this.Values = line.Split(',').Select(x => int.Parse(x)).ToList();
+        }
+
+        public int GetValue(int idx)
+        {
+            return Values[idx];
+        }
+
+        public bool IsValid(TicketManager manager)
+        {
+            return Values.All(v => manager.Ranges.ContainsKey(v));
+        }
+
+        public IEnumerable<int> GetInvalidValues(TicketManager manager)
+        {
+            return Values.Where(x => !manager.Ranges.ContainsKey(x));
+        }
+    }
+
+    public class TicketManager
+    {
+        public List<string> Source { get; } = null;
+
+        public Ticket Mine { get; } = null;
+        public List<Ticket> Nearby { get; } = new List<Ticket>();
+        public List<TicketNote> Notes { get; } = new List<TicketNote>();
+
+        public Dictionary<int, HashSet<string>> Ranges { get; private set; } = null;
+
+        public TicketManager(IEnumerable<string> input)
+        {
+            this.Source = input.ToList();
+
+            int blankCounter = 0;
+            foreach (var line in input)
+            {
+                if (line == "")
+                {
+                    blankCounter++;
+                }
+                else if (!line.EndsWith(":"))
+                {
+                    switch (blankCounter)
+                    {
+                        case 0:
+                            Notes.Add(new TicketNote(line));
+                            break;
+                        case 1:
+                            Mine = new Ticket(line);
+                            break;
+                        case 2:
+                            Nearby.Add(new Ticket(line));
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+
+            PopulateRangeMap();
+        }
+
+        private void PopulateRangeMap()
+        {
+            Ranges = new Dictionary<int, HashSet<string>>();
+            foreach (var note in Notes)
+            {
+                foreach (var range in note.Ranges)
+                {
+                    int len = (range[1] - range[0]) + 1;
+                    foreach (int i in Enumerable.Range(range[0], len))
+                    {
+                        if (!Ranges.ContainsKey(i))
+                        {
+                            Ranges.Add(i, new HashSet<string>());
+                        }
+                        Ranges[i].Add(note.Name);
+                    }
+                }
+            }
+        }
+
+        public int GetScanningErrorRate()
+        {
+            var vals = new List<int>();
+            foreach (var ticket in Nearby)
+            {
+                var invalid = ticket.GetInvalidValues(this);
+                if (invalid.Count() > 0)
+                {
+                    vals.AddRange(invalid);
+                }
+            }
+            return vals.Sum();
+        }
+
+        public Dictionary<int, string> GetValidFieldPositions()
+        {
+            var validTix = Nearby.Where(x => x.IsValid(this));
+
+            var usedFields = new HashSet<string>();
+            var positions = new Dictionary<int, string>();
+
+            int ticketCount = validTix.Count();
+
+            //< Need to solve for each position within the tickets
+            while (usedFields.Count != Mine.Length)
+            {
+                for (int i = 0; i < Mine.Length; i++)
+                {
+                    if (positions.ContainsKey(i))
+                    {
+                        //< Already solved - skip
+                        continue;
+                    }
+
+                    //< Get all the values at this position in the ticket from nearby, valid tix
+                    var vals = validTix.Select(x => x.GetValue(i));
+                    //< Get the map of number of times a note covered this index in nearby, valid tix
+                    var noteMap = GetNoteMap(vals);
+                    //< Get the matching notes (notes which were covered by all tix)
+                    var matching = noteMap.Where(kvp => !usedFields.Contains(kvp.Key))
+                                          .Where(kvp => kvp.Value == ticketCount)
+                                          .ToList();
+                    //< Switch on the resulting count - if more than one, need to solve other positions first
+                    if (matching.Count() > 1)
+                    {
+                        //< Can't decide yet - retain for later check
+                    }
+                    else
+                    {
+                        //< Only one solution here - use it and move on
+                        var match = matching.Single();
+                        positions.Add(i, match.Key);
+                        usedFields.Add(match.Key);
+                    }
+                }
+            }
+
+            return positions;
+        }
+
+        private Dictionary<string, int> GetNoteMap(IEnumerable<int> vals)
+        {
+            var noteMap = new Dictionary<string, int>();
+            foreach (var v in vals)
+            {
+                foreach (var note in Ranges[v])
+                {
+                    if (!noteMap.ContainsKey(note))
+                    {
+                        noteMap.Add(note, 0);
+                    }
+                    noteMap[note]++;
+                }
+            }
+            return noteMap;
+        }
+
+        public Dictionary<string, int> GetMyTicketValues(Dictionary<int, string> fieldPositions)
+        {
+            var map = new Dictionary<string, int>();
+            foreach (var kvp in fieldPositions)
+            {
+                map.Add(kvp.Value, Mine.GetValue(kvp.Key));
+            }
+            return map;
+        }
+    }
+}
